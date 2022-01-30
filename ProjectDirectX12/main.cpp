103aadafdee27f6ed50954311417a3cfb01d179c
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2022-01-30 17:15:14 +0900

    ウィンドウの表示

--- ProjectDirectX12/main.cpp
+++ ProjectDirectX12/main.cpp
@@ -1,6 +1,17 @@
+#include"window.hpp"
+#include<utility>
 
 int main()
 {
+	constexpr std::size_t WINDOW_WIDTH = 500;
+	constexpr std::size_t WINDOW_HEIGHT = 500;
+
+	auto hwnd = pdx12::create_window(L"window", WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	while (pdx12::update_window())
+	{
+
+	}
 
 	return 0;
 }
