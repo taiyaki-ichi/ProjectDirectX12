fa65c7cc4cd529bd995fbc02eaa36fde27488ae4
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2022-06-04 13:43:52 +0900

    root_signatureの分離

--- ProjectDirectX12/root_signature.hpp
+++ ProjectDirectX12/root_signature.hpp
@@ -4,7 +4,6 @@
 #include<vector>
 #include<array>
 
-#pragma comment(lib,"d3d12.lib")
 
 namespace pdx12
 {
@@ -29,111 +28,6 @@ namespace pdx12
 		UINT num = 1;
 	};
 	
-	inline release_unique_ptr<ID3D12RootSignature> create_root_signature(ID3D12Device* device,
-		std::vector<std::vector<descriptor_range_type>> const& descriptorRangeTypes, const std::vector<static_sampler>& staticSamplers)
-	{
-		std::vector<std::vector<D3D12_DESCRIPTOR_RANGE>> descriptorRanges{};
-
-		//配列の構造をもとにレジスタの番号を振っりD3D12_DESCRIPTOR_RANGEを作っていく
-		{
-			descriptorRanges.reserve(descriptorRangeTypes.size());
-
-			std::array<std::uint32_t, 4> registerNums{};
-
-			for (auto& rangeTypes : descriptorRangeTypes) {
-				std::vector<D3D12_DESCRIPTOR_RANGE> ranges{};
-				ranges.reserve(rangeTypes.size());
-
-				for (auto& t : rangeTypes) {
-					D3D12_DESCRIPTOR_RANGE descriptorRange{};
-					descriptorRange.NumDescriptors = t.num;
-					descriptorRange.RangeType = static_cast<D3D12_DESCRIPTOR_RANGE_TYPE>(t.descriptorRangeType);
-					descriptorRange.BaseShaderRegister = registerNums[static_cast<D3D12_DESCRIPTOR_RANGE_TYPE>(t.descriptorRangeType)];
-					descriptorRange.OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
-
-					ranges.push_back(std::move(descriptorRange));
-					registerNums[static_cast<D3D12_DESCRIPTOR_RANGE_TYPE>(t.descriptorRangeType)] += t.num;
-				}
-
-				descriptorRanges.push_back(std::move(ranges));
-			}
-		}
-
-
-		std::vector<D3D12_ROOT_PARAMETER> descriptorTables{};
-
-		//descriptorRangesをもとにディスクリプタテーブルを作成
-		{
-			descriptorTables.reserve(descriptorRanges.size());
-
-			for (auto& r : descriptorRanges) {
-				D3D12_ROOT_PARAMETER tmp{};
-				tmp.ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
-				tmp.DescriptorTable.pDescriptorRanges = r.data();
-				tmp.DescriptorTable.NumDescriptorRanges = r.size();
-				tmp.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
-
-				descriptorTables.push_back(std::move(tmp));
-			}
-		}
-
-
-		std::vector<D3D12_STATIC_SAMPLER_DESC> staticSamplerDescs(staticSamplers.size());
-
-		{
-			for (std::uint32_t i = 0; i < staticSamplers.size(); i++)
-			{
-				staticSamplerDescs[i].Filter = staticSamplers[i].filter;
-				staticSamplerDescs[i].AddressU = staticSamplers[i].address_u;
-				staticSamplerDescs[i].AddressV = staticSamplers[i].address_v;
-				staticSamplerDescs[i].AddressW = staticSamplers[i].address_w;
-				staticSamplerDescs[i].ComparisonFunc = staticSamplers[i].comparison_func;
-
-				staticSamplerDescs[i].MaxAnisotropy = staticSamplers[i].max_anisotropy;
-
-				staticSamplerDescs[i].MinLOD = staticSamplers[i].min_LOD;
-				staticSamplerDescs[i].MaxLOD = staticSamplers[i].max_LOD;
-
-				staticSamplerDescs[i].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
-				staticSamplerDescs[i].ShaderRegister = i;
-			}
-		}
-
-
-
-		D3D12_ROOT_SIGNATURE_DESC rootSignatureDesc = {};
-		rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
-		rootSignatureDesc.NumParameters = descriptorTables.size();
-		rootSignatureDesc.pParameters = descriptorTables.size() > 0 ? descriptorTables.data() : nullptr;
-		rootSignatureDesc.NumStaticSamplers = staticSamplerDescs.size();
-		rootSignatureDesc.pStaticSamplers = staticSamplerDescs.size() > 0 ? staticSamplerDescs.data() : nullptr;
-
-
-		ID3DBlob* rootSigBlob = nullptr;
-		ID3DBlob* errorBlob = nullptr;
-		release_unique_ptr<ID3D12RootSignature> result{};
-
-		if (D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1_0, &rootSigBlob, &errorBlob))
-		{
-			std::string errstr{};
-			errstr.resize(errorBlob->GetBufferSize());
-			std::copy_n((char*)errorBlob->GetBufferPointer(), errorBlob->GetBufferSize(), errstr.begin());
-			errorBlob->Release();
-
-			THROW_PDX12_EXCEPTION(errstr.data());
-		}
-
-		ID3D12RootSignature* tmp = nullptr;
-		if (FAILED(device->CreateRootSignature(0, rootSigBlob->GetBufferPointer(), rootSigBlob->GetBufferSize(), IID_PPV_ARGS(&tmp))))
-		{
-			THROW_PDX12_EXCEPTION("failed CreateRootSignature");
-		}
-
-		result.reset(tmp);
-		rootSigBlob->Release();
-
-		return result;
-	}
-
-
+	release_unique_ptr<ID3D12RootSignature> create_root_signature(ID3D12Device* device,
+		std::vector<std::vector<descriptor_range_type>> const& descriptorRangeTypes, const std::vector<static_sampler>& staticSamplers);
 }
