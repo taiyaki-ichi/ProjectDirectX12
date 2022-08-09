7f16836e7015b791f3553a799a055140895b3d4b
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2022-08-09 11:55:15 +0900

    descriptor_heapの修正

--- ProjectDirectX12/descriptor_heap.hpp
+++ ProjectDirectX12/descriptor_heap.hpp
@@ -12,11 +12,14 @@ namespace pdx12
 		release_unique_ptr<ID3D12DescriptorHeap> ptr{};
 
 		std::uint32_t size = 0;
-		std::uint32_t increment_size = 0;
+		// あるハンドルから次のハンドルまでの距離のキャッシュ用
+		// ディスクリプタヒープのタイプによってハンドル間の距離が異なる
+		std::uint32_t increment_handle_size = 0;
 
 	public:
 		void initialize(ID3D12Device* device, D3D12_DESCRIPTOR_HEAP_TYPE type, UINT size);
 
+		// index番目のハンドルを返す
 		D3D12_GPU_DESCRIPTOR_HANDLE get_GPU_handle(std::size_t index = 0);
 		D3D12_CPU_DESCRIPTOR_HANDLE get_CPU_handle(std::size_t index = 0);
 
